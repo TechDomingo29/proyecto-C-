9b42b3fbefe357fccc936914e32de4ded8291e87
Carlos Ruiz <ruizc7574@gmail.com>
2024-05-19 23:24:12 -0400

    proyectoC#2

--- area_perimetro/area_perimetro/Program.cs
+++ area_perimetro/area_perimetro/Program.cs
@@ -1,15 +1,15 @@
-﻿  Console.WriteLine("Calculadora de Área y Perímetro de un Rectángulo");
+﻿Console.WriteLine("Calculadora de Área y Perímetro de un Rectángulo");
 
-        Console.Write("Ingrese la longitud del rectángulo: ");
-        double longitud = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese la longitud del rectángulo: ");
+double longitud = Convert.ToDouble(Console.ReadLine());
 
-        Console.Write("Ingrese la anchura del rectángulo: ");
-        double anchura = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ingrese la anchura del rectángulo: ");
+double anchura = Convert.ToDouble(Console.ReadLine());
 
         
-        double area = longitud * anchura;
-        double perimetro = 2 * (longitud + anchura);
+double area = longitud * anchura;
+double perimetro = 2 * (longitud + anchura);
 
-        Console.WriteLine($"El área del rectángulo es: {area}");
-        Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
+Console.WriteLine($"El área del rectángulo es: {area}");
+Console.WriteLine($"El perímetro del rectángulo es: {perimetro}");
     
