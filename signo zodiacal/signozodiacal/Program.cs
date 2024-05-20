7799b56f48aa1e41af8c2cd5b089ffa43dcba737
Carlos Ruiz <ruizc7574@gmail.com>
2024-05-19 23:21:33 -0400

    proyectoC#

---
+++ signo zodiacal/signozodiacal/Program.cs
@@ -0,0 +1,18 @@
+﻿
+        Console.WriteLine("Calculadora de Signo Zodiacal");
+
+        Console.Write("Ingrese su mes de nacimiento (número): ");
+        int mesNacimiento = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese su día de nacimiento: ");
+        int diaNacimiento = Convert.ToInt32(Console.ReadLine());
+
+        string[] signosZodiaco = {
+            "Capricornio", "Acuario", "Piscis", "Aries", "Tauro", "Géminis",
+            "Cáncer", "Leo", "Virgo", "Libra", "Escorpio", "Sagitario"
+        };
+
+        int signoIndex = (mesNacimiento - 1) % 12;
+        string signoZodiacal = signosZodiaco[signoIndex];
+
+        Console.WriteLine($"Su signo zodiacal es: {signoZodiacal}");
---
+++ simple calculadora/calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿  Console.WriteLine("Calculadora Simple");
+        
+        Console.Write("Ingrese el primer número: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.Write("Ingrese el segundo número: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+        
+        Console.WriteLine("Seleccione la operación:");
+        Console.WriteLine("1 - Suma");
+        Console.WriteLine("2 - Resta");
+        Console.WriteLine("3 - Multiplicación");
+        Console.WriteLine("4 - División");
+        
+        int opcion = Convert.ToInt32(Console.ReadLine());
+        double resultado = 0;
+
+        switch (opcion)
+        {
+            case 1:
+                resultado = num1 + num2;
+                break;
+            case 2:
+                resultado = num1 - num2;
+                break;
+            case 3:
+                resultado = num1 * num2;
+                break;
+            case 4:
+                if (num2 != 0)
+                {
+                    resultado = num1 / num2;
+                }
+                else
+                {
+                    Console.WriteLine("Error: No se puede dividir por cero.");
+                }
+                break;
+            default:
+                Console.WriteLine("Opción inválida.");
+                break;
+        }
+        
+        Console.WriteLine("El resultado es: " + resultado);
