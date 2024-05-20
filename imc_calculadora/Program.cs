1104f0994cc1b7bc44b3729ca84908db232e5295
Carlos Ruiz <ruizc7574@gmail.com>
2024-05-19 23:34:15 -0400

    proyectoC#4

---
+++ imc_calculadora/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Calculadora de Índice de Masa Corporal (IMC)");
+
+Console.Write("Ingrese su peso en kilogramos: ");
+double peso = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Ingrese su altura en metros: ");
+double altura = Convert.ToDouble(Console.ReadLine());
+
+        
+double imc = peso / (altura * altura);
+
+Console.WriteLine($"Su Índice de Masa Corporal (IMC) es: {imc}");
+
+        
+    if (imc < 18.5)
+        {
+        Console.WriteLine("Peso insuficiente");
+        }
+        else if (imc >= 18.5 && imc < 25)
+        {
+            Console.WriteLine("Peso normal");
+        }
+        else if (imc >= 25 && imc < 30)
+        {
+            Console.WriteLine("Sobrepeso");
+        }
+        else
+        {
+            Console.WriteLine("Obesidad");
+        }
+ Console.Write("Presiona una tecla para terminar . . . ");
+ Console.ReadKey();
